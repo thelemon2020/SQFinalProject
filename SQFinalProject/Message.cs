c804a087b406daf44443d25a89955d598a64392e
thelemon2020 <53379286+thelemon2020@users.noreply.github.com>
2020-10-19 13:32:56 -0400

    Add files via upload

---
+++ SQFinalProject/Message.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQFinalProject
+{
+    class Message
+    {
+        Comm newComm = new Comm();
+        Customer newCust = new Customer();
+        Transpo newTranspo = new Transpo();
+        UI newUI = new UI();
+    }
+}
