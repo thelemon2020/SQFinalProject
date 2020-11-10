22921d9847b66953830fec24703a86e4e1c7127f
thelemon2020 <53379286+thelemon2020@users.noreply.github.com>
2020-11-10 08:15:35 -0500

    added sprint 1 stuff

---
+++ DBComm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace SQFinalProject
+{
+    public class DBComm
+    {
+        public string connectionString { get; set; }
+        public MySqlConnection currentConnection { get; set; }
+        public MySqlCommand SQLCommand { get; set; }
+        public string userCommand { get; set; }
+        public List<string> SQLReturn { get; set; }
+
+        public DBComm(string dbIP, string userName, string password, string command, string table)
+        {
+            connectionString = "server=" + dbIP + ";uid=" + userName + ";pwd=" + password + ";database=" + table;
+            userCommand = command;
+        }
+
+        public List<string> ExecuteCommand()
+        {
+            currentConnection = DatabaseInteraction.connectToDatabase(connectionString);
+            SQLCommand = new MySqlCommand(userCommand, currentConnection);
+            SQLReturn = DatabaseInteraction.CommandDatabase(SQLCommand);
+            DatabaseInteraction.CloseConnection(currentConnection);
+            return SQLReturn;
+        }
+    }
+}
