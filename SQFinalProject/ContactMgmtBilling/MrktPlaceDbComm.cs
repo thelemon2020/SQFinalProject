124712d720b19692d40cfee8f135eb8e1ed00848
Tangent1al <y.dydxx.b@gmail.com>
2020-11-10 08:52:36 -0500

    Added in my classes

---
+++ SQFinalProject/ContactMgmtBilling/MrktPlaceDbComm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQFinalProject.ContactMgmtBilling
+{
+    public class MrktPlaceDbComm :DBComm
+    {
+        public MrktPlaceDbComm(string dbIP, string userName, string password, string command, string table) : base(dbIP, userName, password, command, table)
+        {
+            
+        }
+
+        public ContractDetails SelectContract()
+        {
+            // Get a contract from the database
+        }
+
+        public List<ContractDetails> ShowActiveContracts()
+        {
+            // return a list populated with active contracts
+        }
+    }
+}
