d39d7ea8eae178cecf24d6addef8a203bf55180a
thelemon2020 <53379286+thelemon2020@users.noreply.github.com>
2020-11-16 14:30:07 -0500

    Add files via upload
    new versions to connect to db

---
+++ SQFinalProject/Database.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace SQFinalProject
+{
+    public class Database
+    {
+        public string connectionString { get; set; }
+
+        public string ip { get; set; }
+        public string user { get; set; }
+        public string pass { get; set; }
+        public string schema { get; set; }
+        public MySqlConnection currentConnection { get; set; }
+        public MySqlCommand SQLCommand { get; set; }
+        public string userCommand { get; set; }
+        public List<string> SQLReturn { get; set; }
+
+        public Database(string dbIP, string userName, string password, string table)
+        {
+            ip = dbIP;
+            user = userName;
+            pass = password;
+            schema = table;
+            connectionString = "server=" + ip + ";uid=" + user + ";pwd=" + pass + ";database=" + schema;
+        }
+
+        public List<string> ExecuteCommand()
+        {
+            currentConnection = DatabaseInteraction.connectToDatabase(connectionString);
+            SQLCommand = new MySqlCommand(userCommand, currentConnection);
+            SQLReturn = DatabaseInteraction.CommandDatabase(SQLCommand);
+            DatabaseInteraction.CloseConnection(currentConnection);
+            return SQLReturn;
+        }
+
+        public void MakeCommand(string table, string searchParam)
+        {
+            userCommand = "SELECT" + searchParam + "FROM" + table;
+        }
+    }
+}
