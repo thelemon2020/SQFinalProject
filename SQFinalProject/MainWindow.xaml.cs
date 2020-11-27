a5869e707af1cee3aa92e9d9ec1e54e06735769d
DericKruse <71707679+DericKruse@users.noreply.github.com>
2020-11-27 15:55:07 -0500

    Fixed commenting
    - Added file/class header comments to the files I was working on
    - Made sure there was adaquate Doxygen readable in-line comments within the methods
    - Touchups on the About window

--- SQFinalProject/MainWindow.xaml.cs
+++ SQFinalProject/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -15,37 +16,38 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using SQFinalProject.ContactMgmtBilling;
 
-namespace SQFinalProject
-{
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+namespace SQFinalProject {
+    ///
+    /// \class <b>MainWindow</b>
+    /// 
+    /// \brief This class holds all the event handlers for for the main WPF window.  It has data members for the TMS database & the Marketplace database.
+    /// 
+    /// \author <i>Deric Kruse, Chris Lemon</i>
+    /// 
     public partial class MainWindow : Window
     {
-        public const string configFilePath = @"..\..\config\TMS.txt";
-        public List<string> TMS_Database { get; set; }
-        public List<string> MarketPlace_Database { get; set; }
-        Database loginDB { get; set; }
-        Database MarketPlace { get; set; }
+        //! Properties
+        public const string configFilePath = @"..\..\config\TMS.txt";   //<The path to the config file
+        public List<string> TMS_Database { get; set; }                  //<The the string list to store TMS DB connection info
+        public List<string> MarketPlace_Database { get; set; }          //<The the string list to store Marketplace DB connection info
+        Database loginDB { get; set; }                                  //<The database object for the TMS database
+        Database MarketPlace { get; set; }                              //<The database object for the Marketplace database
 
-        public string userName;
-
-        private bool isLoggedIn = false;
+        public string userName;                                         //<Stores the user name of the current user
 
         public MainWindow ( string name )
         { 
             InitializeComponent();
-            LoadConfig();
-            if (TMS_Database!=null)
+            LoadConfig();                                               //< Parse the config file
+            if (TMS_Database!=null)                                     //< Connect to the TMS database if the config file loaded successfully
             {
                 loginDB = new Database(TMS_Database[0], TMS_Database[1], TMS_Database[2], TMS_Database[3]);
             }
-            if (MarketPlace_Database!=null)
+            if (MarketPlace_Database!=null)                             //< Connect to the Marketplace database if the config file loaded successfully
             {
                 MarketPlace = new Database(MarketPlace_Database[0], MarketPlace_Database[1], MarketPlace_Database[2], MarketPlace_Database[3]);
             }
 
-            isLoggedIn = true;
             userName = name;
             lblUsrInfo.Content = "User Name:  " + userName;
         }
@@ -63,18 +65,18 @@ namespace SQFinalProject
         /// 
         public void LoadConfig()
         {
-            if (File.Exists(configFilePath))
+            if (File.Exists(configFilePath))                        //< If the config file exists, try to read from it
             {
                 StreamReader configFile = new StreamReader(configFilePath);
                 string contents = configFile.ReadToEnd();
                 configFile.Close();
                 if (contents != "")
                 {
-                    string[] splitByDB = contents.Split('\n');
+                    string[] splitByDB = contents.Split('\n');      //< Grab each line so it can be dealt with individually
                     foreach (string dbDetails in splitByDB)
                     {
-                        string[] details = dbDetails.Split(' ');
-                        if (details[0] == "TMS")
+                        string[] details = dbDetails.Split(' ');    //< Pull the individual fields from the line of the config file
+                        if (details[0] == "TMS")                    //< If the line pertains to the TMS database, assign the values to the TMS string list
                         {
                             TMS_Database = new List<string>();
                             for (int i = 1; i < details.Count(); i++)
@@ -82,7 +84,7 @@ namespace SQFinalProject
                                 TMS_Database.Add(details[i]);
                             }
                         }
-                        else if (details[0] == "MP")
+                        else if (details[0] == "MP")                //< If the line pertains to the Marketplace database, assign the values to the Marketplace string list
                         {
                             MarketPlace_Database = new List<string>();
                             for (int i = 1; i < details.Count(); i++)
@@ -102,17 +104,17 @@ namespace SQFinalProject
 
 
 
-        //  METHOD:	    Window_Loaded
-        /// \brief Method that is called when the main window is loaded
-        /// \details <b>Details</b>
-        ///     The Window_Loaded event gets called when the window is finished loading.  
-        /// It brings up the Login window and handles what happens when the login window closes.
-        /// 
-        /// \param - <b>sender:</b>  the object that called the method
-        /// \param - <b>e:</b>       the arguments that are passed when this method is called
-        /// 
-        /// \return - <b>Nothing</b>
-        ///
+        //  METHOD:	    Window_Loaded, not currently used
+        // \brief Method that is called when the main window is loaded
+        // \details <b>Details</b>
+        //     The Window_Loaded event gets called when the window is finished loading.  
+        // It brings up the Login window and handles what happens when the login window closes.
+        // 
+        // \param - <b>sender:</b>  the object that called the method
+        // \param - <b>e:</b>       the arguments that are passed when this method is called
+        // 
+        // \return - <b>Nothing</b>
+        //
        /* private void Window_Loaded ( object sender, EventArgs e ) {
             LoginWindow initialLogin = new LoginWindow ( loginDB );
             initialLogin.Owner = this;
@@ -131,11 +133,10 @@ namespace SQFinalProject
 
 
 
-        //  METHODS:	CloseCB_CanExecute & CloseCB_Executed
-        /// \brief Says when the close command can run and what happens when it does
+        //  METHOD:	CloseCB_CanExecute
+        /// \brief Says when the close command can run, which is always
         /// \details <b>Details</b>
-        ///     The CanExecute method says when the Close command binding can function and the Executed method says what happens
-        /// when it is run.  If Close is clicked, close the window.
+        ///     The CanExecute method says when the Close command binding can function, which is always.
         /// 
         /// \param - <b>sender:</b>  the object that called the method
         /// \param - <b>e:</b>       the arguments that are passed when this method is called
@@ -146,6 +147,18 @@ namespace SQFinalProject
             e.CanExecute = true;
         }
 
+
+
+        //  METHOD:	CloseCB_Executed
+        /// \brief Dictates what happens when the close command runs
+        /// \details <b>Details</b>
+        ///     The Executed method says what happens the Close Command is run.  If Close is clicked, close the window.
+        /// 
+        /// \param - <b>sender:</b>  the object that called the method
+        /// \param - <b>e:</b>       the arguments that are passed when this method is called
+        /// 
+        /// \return - <b>Nothing</b>
+        ///
         private void CloseCB_Executed ( object sender,ExecutedRoutedEventArgs e ) {
             this.Close();
         }
@@ -186,16 +199,34 @@ namespace SQFinalProject
             aboutBox.ShowDialog();
         }
 
+
+        
+        public ObservableCollection<string> MarketRtn;
+
+        //  METHOD:		GetContracts
+        /// \brief This method gets the currently available contracts from the marketplace and displays them to the screen.
+        /// \details <b>Details</b>
+        ///     Creates and sends a query to the Marketplace database then populates a container which updates the information in 
+        /// the list box on the screen.
+        /// 
+        /// \param - <b>sender:</b>  the object that called the method
+        /// \param - <b>e:</b>       the arguments that are passed when this method is called
+        /// 
+        /// \return - <b>Nothing</b>
+        ///
         private void GetContracts ( object sender,RoutedEventArgs e ) {
-            List<string> QueryLst =  new List<string> ();
+            List<string> QueryLst = new List<string> ();
             QueryLst.Add ("*");
 
-            Dictionary<string, string> tempDict = new Dictionary<string, string>();
+            //Dictionary<string, string> tempDict = new Dictionary<string, string>();
             //tempDict.Add ("username", usrName);
 
-            MarketPlace.MakeSelectCommand ( QueryLst, "cmp", tempDict );
+            loginDB.MakeSelectCommand ( QueryLst, "login", null );
+            
+            List<string> tmp = loginDB.ExecuteCommand();
+            //txtMain.Text = tmp.ElementAt(0) + tmp.ElementAt(1) + tmp.ElementAt(2);
                             
-            List<string> PassReturn = MarketPlace.ExecuteCommand();
+            MarketRtn = new ObservableCollection <string> ( loginDB.ExecuteCommand() );
         }
 
         private void test(object sender, MouseButtonEventArgs e)
