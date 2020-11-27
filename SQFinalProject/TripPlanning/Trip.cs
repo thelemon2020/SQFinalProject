6d96ab4aeb33056e22c4e66c3b16fd693f4e239f
lardmonkey65 <64053664+lardmonkey65@users.noreply.github.com>
2020-11-27 04:05:14 -0500

    Added Trip/TripLine classes and properties
    still need constructors and various methods

---
+++ SQFinalProject/TripPlanning/Trip.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQFinalProject.TripPlanning
+{
+    class Trip
+    {
+        public int TripID { get; set; }
+        public int CarrierID { get; set; }
+        public string Origin { get; set; }
+        public string Destination { get; set; }
+        public string VanType { get; set; }
+        public int Distance { get; set; }
+        public int Rate { get; set; }
+        public List<TripLine> Contracts { get; set; }
+
+        public Trip()
+        {
+            
+        }
+
+    }
+}
---
+++ SQFinalProject/TripPlanning/TripLine.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQFinalProject.TripPlanning
+{
+    class TripLine
+    {
+        public int ContractID { get; set; }
+        public int TripID { get; set; }
+        public int Quantity { get; set; }
+        public string Destination { get; set; }
+        public int EstTime { get; set; }
+
+        public TripLine(int contract, int trip, int qty)
+        {
+            ContractID = contract;
+            TripID = trip;
+            Quantity = qty;
+            EstTime = 0;
+        }
+    }
+}
