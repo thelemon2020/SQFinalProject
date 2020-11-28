db973739f00e0f83437f75837507229f58ed8bbd
Tangent1al <y.dydxx.b@gmail.com>
2020-11-27 19:23:34 -0500

    Trip is no more

--- SQFinalProject/TripPlanning/Truck.cs
+++ SQFinalProject/TripPlanning/Truck.cs
@@ -8,6 +8,20 @@ namespace SQFinalProject.TripPlanning
 {
     class Truck
     {
+        public int TripID { get; set; }
+        public int CarrierID { get; set; }
+        public string Origin { get; set; }
+        public string Destination { get; set; }
+        public string VanType { get; set; }
+        public double Rate { get; set; }
+        public double ReeferRate { get; set; }
+        public List<TripLine> Contracts { get; set; }
+
+
+        public Truck()
+        {
+            
+        }
 
     }
 }
