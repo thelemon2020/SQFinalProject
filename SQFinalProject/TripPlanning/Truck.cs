af0c4bbc95dce5a7169fe151746987f1c08e92c2
Tangent1al <y.dydxx.b@gmail.com>
2020-11-27 19:35:35 -0500

    commit

--- SQFinalProject/TripPlanning/Truck.cs
+++ SQFinalProject/TripPlanning/Truck.cs
@@ -8,12 +8,14 @@ namespace SQFinalProject.TripPlanning
 {
     class Truck
     {
+        private const int kMaxPallets = 26;
         public int TripID { get; set; }
         public int CarrierID { get; set; }
         public string Origin { get; set; }
         public string Destination { get; set; }
         public string VanType { get; set; }
         public double Rate { get; set; }
+        public double HoursWorked { get; set; }
         public double ReeferRate { get; set; }
         public List<TripLine> Contracts { get; set; }
 
@@ -23,5 +25,24 @@ namespace SQFinalProject.TripPlanning
             
         }
 
+        public void ContinueRoute()
+        {
+            // Move to the next city and simulate the passage of time
+            
+        }
+
+        public void Load(TripLine contract)
+        {
+            // minus 2 hours from work day
+            // add new contract to List
+            // update quantity of pallets
+        }
+
+        public void Unload(TripLine contract)
+        {
+            // minus 2 hours from the work day
+            // remove contract from list
+            // update quantity of pallets
+        }
     }
 }
