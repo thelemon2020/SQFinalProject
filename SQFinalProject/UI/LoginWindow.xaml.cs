1c2a533a14b47309cef4c2d04e960f33aa287f27
DericKruse <71707679+DericKruse@users.noreply.github.com>
2020-12-07 23:44:03 -0500

    Added file headder comments

--- SQFinalProject/UI/LoginWindow.xaml.cs
+++ SQFinalProject/UI/LoginWindow.xaml.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿//*********************************************
+// File			 : LoginWindow.xaml.cs
+// Project		 : PROG2020 - Term Project
+// Programmer	 : Nick Byam, Chris Lemon, Deric Kruse, Mark Fraser
+// Last Change   : 2020-12-06
+//*********************************************
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
